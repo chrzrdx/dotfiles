807aefadf70b4916ab53a89f33600a02c5aa3b42
Abhinandan Panigrahi <chrzrdx@gmail.com>
2016-04-13 04:46:15 +0530

    added fzf bindings

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -5,21 +5,21 @@
 ex() {
     if [[ -f $1 ]]; then
         case $1 in
-          *.tar.bz2) tar xvjf $1;;
-          *.tar.gz) tar xvzf $1;;
-          *.tar.xz) tar xvJf $1;;
-          *.tar.lzma) tar --lzma xvf $1;;
-          *.bz2) bunzip $1;;
-          *.rar) unrar e $1;;
-          *.gz) gunzip $1;;
-          *.tar) tar xvf $1;;
-          *.tbz2) tar xvjf $1;;
-          *.tgz) tar xvzf $1;;
-          *.zip) unzip $1;;
-          *.Z) uncompress $1;;
-          *.7z) 7z x $1;;
-          *) echo "'$1' cannot be extracted via >ex<";;
-    esac
+            *.tar.bz2) tar xvjf $1;;
+            *.tar.gz) tar xvzf $1;;
+            *.tar.xz) tar xvJf $1;;
+            *.tar.lzma) tar --lzma xvf $1;;
+            *.bz2) bunzip $1;;
+            *.rar) unrar e $1;;
+            *.gz) gunzip $1;;
+            *.tar) tar xvf $1;;
+            *.tbz2) tar xvjf $1;;
+            *.tgz) tar xvzf $1;;
+            *.zip) unzip $1;;
+            *.Z) uncompress $1;;
+            *.7z) 7z x $1;;
+            *) echo "'$1' cannot be extracted via >ex<";;
+        esac
     else
         echo "'$1' is not a valid file"
     fi
@@ -45,18 +45,62 @@ any() {
 # display a neatly formatted path
 # -------------------------------------------------------------------
 path() {
-  echo $PATH | tr ":" "\n" | \
-    awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
-           sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
-           sub(\"/opt\",   \"$fg_no_bold[cyan]/opt$reset_color\"); \
-           sub(\"/sbin\",  \"$fg_no_bold[magenta]/sbin$reset_color\"); \
-           sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
-           print }"
+    echo $PATH | tr ":" "\n" | \
+        awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
+        sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
+        sub(\"/opt\",   \"$fg_no_bold[cyan]/opt$reset_color\"); \
+        sub(\"/sbin\",  \"$fg_no_bold[magenta]/sbin$reset_color\"); \
+        sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
+        print }"
 }
 
 # calculator
 
 function calc
 {
-  echo "${1}" | bc -l;
+    echo "${1}" | bc -l;
 }
+
+# fzf configuration
+
+# you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+    local out file key
+    out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+    key=$(head -1 <<< "$out")
+    file=$(head -2 <<< "$out" | tail -1)
+    if [ -n "$file" ]; then
+        [ "$key" = ctrl-o ] && xdg-open "$file" || ${EDITOR:-vim} "$file"
+    fi
+}
+
+# v - search in most recent used files by vim
+v() {
+    local file
+    file=$(sed '1d' $HOME/.cache/neomru/file |
+    fzf --query="$1" --select-1 --exit-0)
+    [ -n "$file" ] && ${EDITOR:-vim} "${file}"
+}
+
+# vd - cd to most recent used directory by vim
+vd() {
+    local dir
+    dir=$(sed '1d' $HOME/.cache/neomru/directory |
+    fzf --query="$1" --select-1 --exit-0) && cd "$dir"
+}
+
+# to quickly ctrl-z in and out of neovim :D
+# https://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/
+fancy-ctrl-z () {
+    if [[ $#BUFFER -eq 0 ]]; then
+        BUFFER="fg"
+        zle accept-line
+    else
+        zle push-input
+        zle clear-screen
+    fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
