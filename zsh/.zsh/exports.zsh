25b37be4222956c6efd01197d055f43e9fd33e86
Abhinandan Panigrahi <chrzrdx@gmail.com>
2016-06-16 03:27:27 +0530

    fixed urxvt scrolling issues, clearlooks + inconsolatazi4 + HighContrast, java export added

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -9,3 +9,6 @@ export VDPAU_DRIVER=va_gl
 export wiki_browser=chromium
 
 export PATH=~/.local/bin:$PATH
+
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+
